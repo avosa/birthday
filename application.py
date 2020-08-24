d9b7888ee6f25772b64b2857a141374365e0db66
avosa <websterb17@gmail.com>
2020-08-24 21:29:59 +0300

    Formatted style

--- application.py
+++ application.py
@@ -4,9 +4,10 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
-	month = datetime.date.today().month
-	day = datetime.date.today().day
+    month = datetime.date.today().month
+    day = datetime.date.today().day
 
-	return render_template("birthday.html", month=month, day=day)
+    return render_template("birthday.html", month=month, day=day)
